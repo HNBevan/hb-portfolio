026fb686493775ced7c292b324dc50e11f15b364
Hayley Bevan <hayleybevan68@gmail.com>
2026-04-13 17:48:43 +0100

    updated video slider

--- js/custom-websites-animations.js
+++ js/custom-websites-animations.js
@@ -1,7 +1,8 @@
 (function () {
         var selectors = [
             '.feature-card', '.pricing-card', '.ideal-card', '.cw-split-card',
-            '.timeline-item', '.faq-item', '.comparison-row:not(.header)'
+            '.timeline-item', '.faq-item', '.comparison-row:not(.header)',
+            '.ecom-platform-card'
         ];
         var dirs = ['srv-from-left', 'srv-from-top', '', 'srv-from-right', 'srv-from-bottom', 'srv-from-left'];
         var elements = Array.from(document.querySelectorAll(selectors.join(',')));
